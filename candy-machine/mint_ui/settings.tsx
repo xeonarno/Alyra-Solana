529785d5ffbca1b743e3bd7df94273d8047b2d50
AurelienMonteillet <aurelien.monteillet@nomadic-labs.com>
2024-07-22 16:18:48 +0200

    Finalize Mint UI and Candy Machine launch

--- candy-machine/mint_ui/settings.tsx
+++ candy-machine/mint_ui/settings.tsx
@@ -4,11 +4,11 @@ export const mintText = [
   { label: "WL", mintText: "WL mint paying with SOL", buttonLabel: "Mint now!", header: "WL Mint" },
   { label: "OG", mintText: "Mint Paying with ABC", buttonLabel: "Mint now!", header: "OG Mint" },
   { label: "publi", mintText: "Mint Paying with DEF", buttonLabel: "Mint now!", header: "Public Mint" },
-  { label: "default", mintText: "default mintText", buttonLabel: "Mint now!", header: "Default"}
+  { label: "default", mintText: "Mint Price : 0.5 sol", buttonLabel: "Mint now!", header: "SolPush" }
 ];
 
 //header image in the ui. replace with your own
-export const image = "https://avatars.githubusercontent.com/u/93528482?v=4";
+export const image = "/collection.png";
 
 //website title
-export const headerText = "MarkSackerberg's mint UI";
+export const headerText = "SolPush";
