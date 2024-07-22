08d5f00cb281c11a211b15077353f970dca5311a
Ahmed Bouchriha <bouchriha.ahmed@gmail.com>
2024-07-22 18:24:18 +0200

    remove uneccessary code

--- sol-push/web/components/account/account-detail-feature.tsx
+++ sol-push/web/components/account/account-detail-feature.tsx
@@ -43,9 +43,7 @@ export default function AccountDetailFeature() {
                     </div>
                 }
             >
-                <div className="my-4">
-                    <AccountButtons address={address} />
-                </div>
+                
             </AppHero>
             <div className="space-y-8">
                 <AccountTokens address={address} />
