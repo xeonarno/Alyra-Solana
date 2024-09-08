e21ec68f49f4fd57150f3c46d3a7cde261aed46e
AurelienMonteillet <aurelien.monteillet@nomadic-labs.com>
2024-09-08 22:12:22 +0200

    add test

---
+++ sol-push/anchor/target/types/click_game.ts
@@ -0,0 +1,252 @@
+/**
+ * Program IDL in camelCase format in order to be used in JS/TS.
+ *
+ * Note that this is only a type helper and is not the actual IDL. The original
+ * IDL can be found at `target/idl/click_game.json`.
+ */
+export type ClickGame = {
+  "address": "5nroogNoBjowEcCqDyPquSAxxp5fPVQ369GRmZghG8xF",
+  "metadata": {
+    "name": "clickGame",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": [
+    {
+      "name": "click",
+      "discriminator": [
+        11,
+        147,
+        179,
+        178,
+        145,
+        118,
+        45,
+        186
+      ],
+      "accounts": [
+        {
+          "name": "game",
+          "writable": true
+        },
+        {
+          "name": "player",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "contractWallet",
+          "writable": true
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": []
+    },
+    {
+      "name": "getGameInfo",
+      "discriminator": [
+        140,
+        141,
+        245,
+        71,
+        227,
+        131,
+        217,
+        93
+      ],
+      "accounts": [
+        {
+          "name": "game"
+        }
+      ],
+      "args": [],
+      "returns": {
+        "defined": {
+          "name": "gameInfo"
+        }
+      }
+    },
+    {
+      "name": "initializeGame",
+      "discriminator": [
+        44,
+        62,
+        102,
+        247,
+        126,
+        208,
+        130,
+        215
+      ],
+      "accounts": [
+        {
+          "name": "game",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "initializer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": [
+        {
+          "name": "owner",
+          "type": "pubkey"
+        }
+      ]
+    },
+    {
+      "name": "withdraw",
+      "discriminator": [
+        183,
+        18,
+        70,
+        156,
+        148,
+        109,
+        161,
+        34
+      ],
+      "accounts": [
+        {
+          "name": "game",
+          "writable": true
+        },
+        {
+          "name": "lastPlayer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": []
+    }
+  ],
+  "accounts": [
+    {
+      "name": "game",
+      "discriminator": [
+        27,
+        90,
+        166,
+        125,
+        74,
+        100,
+        121,
+        18
+      ]
+    }
+  ],
+  "types": [
+    {
+      "name": "game",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "totalLamports",
+            "type": "u64"
+          },
+          {
+            "name": "lastPlayer",
+            "type": "pubkey"
+          },
+          {
+            "name": "lastClickTimestamp",
+            "type": "i64"
+          },
+          {
+            "name": "startTime",
+            "type": "i64"
+          },
+          {
+            "name": "owner",
+            "type": "pubkey"
+          },
+          {
+            "name": "winners",
+            "type": {
+              "array": [
+                {
+                  "option": {
+                    "defined": {
+                      "name": "winner"
+                    }
+                  }
+                },
+                10
+              ]
+            }
+          },
+          {
+            "name": "winnerIndex",
+            "type": "u8"
+          },
+          {
+            "name": "winnerCount",
+            "type": "u8"
+          }
+        ]
+      }
+    },
+    {
+      "name": "gameInfo",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "totalLamports",
+            "type": "u64"
+          },
+          {
+            "name": "remainingTime",
+            "type": "i64"
+          },
+          {
+            "name": "winners",
+            "type": {
+              "vec": {
+                "defined": {
+                  "name": "winner"
+                }
+              }
+            }
+          }
+        ]
+      }
+    },
+    {
+      "name": "winner",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "player",
+            "type": "pubkey"
+          },
+          {
+            "name": "timestamp",
+            "type": "i64"
+          },
+          {
+            "name": "amount",
+            "type": "u64"
+          }
+        ]
+      }
+    }
+  ]
+};
